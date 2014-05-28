62002f6522820bb561acbc238efe5fa2723090dc
Alex Knowles <alexk@datalogics.com>
2014-05-28 08:08:24 -0500

    Add a Driver class to host a main method.
    The main method doesn't do much yet.
    For now, it just prints some messages to the console.
    Includes a TODO to remind us what needs to be done.

---
+++ src/com/datalogics/countdown/Driver.java
@@ -0,0 +1,15 @@
+package com.datalogics.countdown;
+
+public class Driver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, Git!");
+		System.out.println("Let's count down from 100 to 1 and print every number not divisible by 7...");
+
+		// TODO: implement me!
+	}
+
+}
